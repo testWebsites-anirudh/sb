fe5b88e8691afb1dca6bf5a14baef67a8b64479e
aniru <aniru@192.168.0.17>
2020-09-13 14:36:05 -0400

    Practice Commit 1

---
+++ Practice/src/test/java/com/anirudh/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anirudh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
